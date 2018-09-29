be92e39aaac06ed33809d8be6b62499e182fc75b
Shutian Deng <yh2deng@yh2deng-3.local>
2018-09-29 07:50:20 -0400

    update

--- src/dbtest.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = "mongodb+srv://clc:clc2018@db-4yfv6.mongodb.net/test?retryWrites=true";
-
-const userSchema = new mongoose.Schema({ username: 'string', password: 'string', email: 'string', id: 'objectId', participantIds: [id: 'string'], avatar: 'string', status: 'string' });
-const User = mongoose.model('user', userSchema);
-
-mongoose.connect(uri);
